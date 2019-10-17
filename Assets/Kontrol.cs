de8c05e3406238f243ddde8636caf2543ec8f009
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2019-10-18 00:58:12 +0300

    fixed Mouse Rotate

--- Assets/Kontrol.cs
+++ Assets/Kontrol.cs
@@ -6,29 +6,31 @@ using UnityEngine;
 public class Kontrol : MonoBehaviour
 {
     int hiz = 2;
-    void Start()
-    {
-
-    }
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        float mouseInput = Input.GetAxis("Mouse X");
+        Vector3 lookhere = new Vector3(0, mouseInput, 0);
+        transform.Rotate(lookhere*1.5f);
 
-            this.transform.Translate(0, 0, hiz *Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(0, 0, hiz * Time.deltaTime);         
+        }       
 
         if (Input.GetKey(KeyCode.DownArrow))
-
-            this.transform.Translate(0, 0, -1*hiz *Time.deltaTime);
+        {
+            this.transform.Translate(0, 0, -1 * hiz * Time.deltaTime);
+        }
 
         if (Input.GetKey(KeyCode.RightArrow))
-
-            this.transform.Translate(hiz* Time.deltaTime, 0, 0);
+        {
+            this.transform.Translate(hiz * Time.deltaTime, 0, 0);          
+        }         
 
         if (Input.GetKey(KeyCode.LeftArrow))
-
-            this.transform.Translate(-1*hiz * Time.deltaTime, 0, 0);
+        {
+            this.transform.Translate(-1 * hiz * Time.deltaTime, 0, 0);          
+        }
     }
-
 }
-
